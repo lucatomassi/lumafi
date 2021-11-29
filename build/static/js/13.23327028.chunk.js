c7b26e24f27ac981c23231bbced9c1a11ef9cafb
lucatomassi <luca@tomassi.ca>
2021-11-29 14:31:45 -0500

    added to .gitignore

--- build/static/js/13.23327028.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpluma-fi"]=this["webpackJsonpluma-fi"]||[]).push([[13],{1662:function(i,p){}}]);
-//# sourceMappingURL=13.23327028.chunk.js.map
--- build/static/js/14.3c69184c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpluma-fi"]=this["webpackJsonpluma-fi"]||[]).push([[14],{1749:function(i,p){}}]);
-//# sourceMappingURL=14.3c69184c.chunk.js.map
